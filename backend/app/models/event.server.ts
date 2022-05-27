251ac482e08660a3c8ea63c371fb004bf937d4ba
Andrea <andrea@yaaaw.it>
2022-05-28 00:08:53 +0200

    refactor(database models): add models

--- backend/app/models/event.server.ts
+++ backend/app/models/event.server.ts
@@ -1,4 +1,4 @@
-import { Event } from "@prisma/client";
+import { Event, EventOrganizer } from "@prisma/client";
 import { prisma } from "~/helpers/database-helpers";
 
 export const findEvent = async (id: string) => {
@@ -9,8 +9,24 @@ export const findEvent = async (id: string) => {
   });
 };
 
+export const findEventOrganizer = async (id: string) => {
+  return prisma.eventOrganizer.findUnique({
+    where: {
+      id,
+    },
+  });
+};
+
 export const deleteEvent = async (id: string) => {
   return prisma.event.delete({
+    where: {
+      id,
+    },
+  });
+};
+
+export const deleteEventOrganizer = async (id: string) => {
+  return prisma.eventOrganizer.delete({
     where: {
       id,
     },
@@ -28,10 +44,34 @@ export const updateEvent = async (id: string, event: Omit<Event, "id">) => {
   });
 };
 
+export const updateEventOrganizer = async (
+  id: string,
+  eventOrganizer: Omit<EventOrganizer, "id">
+) => {
+  return prisma.eventOrganizer.update({
+    where: {
+      id,
+    },
+    data: {
+      ...eventOrganizer,
+    },
+  });
+};
+
 export const insertEvent = async (event: Omit<Event, "id">) => {
   return prisma.event.create({
     data: {
       ...event,
+    },
+  });
+};
+
+export const insertEventOrganizer = async (
+  eventOrganizer: Omit<EventOrganizer, "id">
+) => {
+  return prisma.eventOrganizer.create({
+    data: {
+      ...eventOrganizer,
     },
   });
 };
--- backend/app/models/user.server.ts
+++ backend/app/models/user.server.ts
@@ -9,8 +9,40 @@ export const findUser = async (id: string) => {
   });
 };
 
+export const findUserLikeEvent = async (userId: string) => {
+  return prisma.userLikeEvent.findMany({
+    where: {
+      userId,
+    },
+  });
+};
+
+export const findUserLikePointOfInterest = async (id: string) => {
+  return prisma.userLikePointOfInterest.findMany({
+    where: {
+      id,
+    },
+  });
+};
+
 export const deleteUser = async (id: string) => {
   return prisma.user.delete({
+    where: {
+      id,
+    },
+  });
+};
+
+export const deleteUserLikeEvent = async (id: string) => {
+  return prisma.userLikeEvent.delete({
+    where: {
+      id,
+    },
+  });
+};
+
+export const deleteUserLikePointOfInterest = async (id: string) => {
+  return prisma.userLikePointOfInterest.delete({
     where: {
       id,
     },
@@ -32,6 +64,43 @@ export const insertUser = async (user: Omit<User, "id">) => {
   return prisma.user.create({
     data: {
       ...user,
+    },
+  });
+};
+
+export const insertUserLikeEvent = async (userId: string, eventId: string) => {
+  return prisma.userLikeEvent.create({
+    data: {
+      user: {
+        connect: {
+          id: userId,
+        },
+      },
+      event: {
+        connect: {
+          id: eventId,
+        },
+      },
+    },
+  });
+};
+
+export const insertUserLikePointOfInterest = async (
+  userId: string,
+  pointOfInterestId: string
+) => {
+  return prisma.userLikePointOfInterest.create({
+    data: {
+      user: {
+        connect: {
+          id: userId,
+        },
+      },
+      pointOfInterest: {
+        connect: {
+          id: pointOfInterestId,
+        },
+      },
     },
   });
 };
