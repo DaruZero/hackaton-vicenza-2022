39b7070d5429b39c236c0c0323a87dc2583d9ae7
Xiper01 <perinxavier01@gmail.com>
2022-05-27 22:43:01 +0200

    Remove unused containers config

--- pipeline_script/ApplicationStart.sh
+++ pipeline_script/ApplicationStart.sh
@@ -5,38 +5,24 @@ cd /home/ec2-user
 ENV="production"
 FRONTEND_URI="hack-frontend"
 BACKEND_URI="hack-backend"
-BACKEND_USERS_URI="hack-backen-users"
-BACKEND_EVENTS_URI="hack-backend-events"
 
 FRONTEND="frontend"
 BACKEND="backend"
-BACKEND_USERS="backen-users"
-BACKEND_EVENTS="backend-events"
 # ENV_FILE="./env_files/prod.env"
-BACKEND_PORT="80"
-FRONTEND_PORT="4001"
-BACKEND_USERS_PORT="4002"
-BACKEND_EVENTS_PORT="4003"
+BACKEND_PORT="3000"
+FRONTEND_PORT="80"
 
 docker stop $FRONTEND
 docker stop $BACKEND
-docker stop $BACKEND_USERS
-docker stop $BACKEND_EVENTS
 
 docker rm $FRONTEND
 docker rm $BACKEND
-docker rm $BACKEND_USERS
-docker rm $BACKEND_EVENTS
 
 docker image prune -f
 aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 495849476805.dkr.ecr.eu-west-1.amazonaws.com
 
 docker pull 495849476805.dkr.ecr.eu-west-1.amazonaws.com/$FRONTEND_URI:$FRONTEND
 docker pull 495849476805.dkr.ecr.eu-west-1.amazonaws.com/$BACKEND_URI:$BACKEND
-docker pull 495849476805.dkr.ecr.eu-west-1.amazonaws.com/$BACKEND_USERS_URI:$BACKEND_USERS
-docker pull 495849476805.dkr.ecr.eu-west-1.amazonaws.com/$BACKEND_EVENTS_URI:$BACKEND_EVENTS
 
 docker run --name $FRONTEND -d --network hack -e SERVER_NAME=$FRONTEND 495849476805.dkr.ecr.eu-west-1.amazonaws.com/$FRONTEND_URI:$FRONTEND
 docker run --name $BACKEND -d --network hack -e SERVER_NAME=$BACKEND 495849476805.dkr.ecr.eu-west-1.amazonaws.com/$BACKEND_URI:$BACKEND
-docker run --name $BACKEND_USERS -d --network hack -e SERVER_NAME=$BACKEND_USERS 495849476805.dkr.ecr.eu-west-1.amazonaws.com/$BACKEND_USERS_URI:$BACKEND_USERS
-docker run --name $BACKEND_EVENTS -d --network hack -e SERVER_NAME=$BACKEND_EVENTS 495849476805.dkr.ecr.eu-west-1.amazonaws.com/$BACKEND_EVENTS_URI:$BACKEND_EVENTS
